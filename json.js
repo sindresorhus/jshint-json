bd445e213599a323759ecfa01c035e91654f37bc
Sindre Sorhus <sindresorhus@gmail.com>
2018-03-25 00:07:59 +0700

    Fix circular structure error
    Fixes #1

--- json.js
+++ json.js
@@ -1,7 +1,9 @@
 'use strict';
+const circularJSON = require('circular-json-es6');
+
 module.exports = {
 	reporter(result, data) {
-		console.log(JSON.stringify({
+		console.log(circularJSON.stringify({
 			result,
 			data
 		}));
